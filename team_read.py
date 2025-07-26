5ddd38676d156662188bfd9ab267977ffdb7b60f
NamikazeAsh <ashwinsatish0@gmail.com>
2025-07-26 21:15:31 +0530

    logo

--- team_read.py
+++ team_read.py
@@ -1,48 +1,3 @@
-def detectRole(team):
-    
-    hazard_moves = {"Stealth Rock", "Spikes", "Toxic Spikes", "Sticky Web"}
-    removal_moves = {"Defog", "Rapid Spin", "Court Change"}
-    setup_moves = {"Swords Dance", "Dragon Dance", "Calm Mind", "Nasty Plot", "Agility", "Bulk Up", "Quiver Dance"}
-    support_moves = {"Wish", "Protect", "Heal Bell", "Aromatherapy", "Thunder Wave", "Taunt", "Toxic", "Leech Seed", "Encore", "Light Screen", "Reflect"}
-    pivot_moves = {"U-turn", "Volt Switch", "Teleport"}
-    recovery_moves = {"Recover", "Roost", "Slack Off", "Moonlight", "Soft-Boiled"}
-    choice_items = {"choice scarf": "Speed Control","choice band": "Physical Breaker","choice specs": "Special Breaker"}
-
-    for poke in team:
-        moves = team[poke]["Moves"]
-        
-        #move based shi
-        if any(move in moves for move in hazard_moves):
-            team[poke]["Roles"].append("Hazard Setter")
-        if any(move in moves for move in removal_moves):
-            team[poke]["Roles"].append("Hazard Remover")
-        if any(move in moves for move in setup_moves):
-            team[poke]["Roles"].append("Setup Sweeper")
-        if any(move in moves for move in support_moves):
-            team[poke]["Roles"].append("Support")
-        if any(move in moves for move in pivot_moves):
-            team[poke]["Roles"].append("Pivot")
-        if any(move in moves for move in recovery_moves):
-            if 'HP' in team[poke]["EVs"] and team[poke]["EVs"].get('Def',0)>100 or team[poke]["EVs"].get('SpD',0)>100:
-                team[poke]["Roles"].append("Recovery")
-        
-        #stat based shi
-        if team[poke]["EVs"].get("Atk", 0) >= 200:
-            team[poke]["Roles"].append("Physical Sweeper")
-        if team[poke]["EVs"].get("SpA", 0) >= 200:
-            team[poke]["Roles"].append("Special Sweeper")
-
-        #item based shi
-        item = team[poke]["Item"].lower()
-        if item in choice_items:
-            team[poke]["Roles"].append(choice_items[item])
-        if item=='Life Orb':
-            team[poke]["Roles"].append("Wallbreaker")
-        
-        
-    print(team)
-    return team
-
 def readTeam(teamRaw):
     team = {}
     teamRaw = teamRaw.strip().split('\n\n')
@@ -85,6 +40,50 @@ def readTeam(teamRaw):
 
         team[i] = poke_data
 
+    return team
+
+def detectRole(team):
+    
+    hazard_moves = {"Stealth Rock", "Spikes", "Toxic Spikes", "Sticky Web"}
+    removal_moves = {"Defog", "Rapid Spin", "Court Change"}
+    setup_moves = {"Swords Dance", "Dragon Dance", "Calm Mind", "Nasty Plot", "Agility", "Bulk Up", "Quiver Dance"}
+    support_moves = {"Wish", "Protect", "Heal Bell", "Aromatherapy", "Thunder Wave", "Taunt", "Toxic", "Leech Seed", "Encore", "Light Screen", "Reflect"}
+    pivot_moves = {"U-turn", "Volt Switch", "Teleport"}
+    recovery_moves = {"Recover", "Roost", "Slack Off", "Moonlight", "Soft-Boiled"}
+    choice_items = {"choice scarf": "Speed Control","choice band": "Physical Breaker","choice specs": "Special Breaker"}
+
+    for poke in team:
+        moves = team[poke]["Moves"]
+        
+        #move based shi
+        if any(move in moves for move in hazard_moves):
+            team[poke]["Roles"].append("Hazard Setter")
+        if any(move in moves for move in removal_moves):
+            team[poke]["Roles"].append("Hazard Remover")
+        if any(move in moves for move in setup_moves):
+            team[poke]["Roles"].append("Setup Sweeper")
+        if any(move in moves for move in support_moves):
+            team[poke]["Roles"].append("Support")
+        if any(move in moves for move in pivot_moves):
+            team[poke]["Roles"].append("Pivot")
+        if any(move in moves for move in recovery_moves):
+            if 'HP' in team[poke]["EVs"] and team[poke]["EVs"].get('Def',0)>100 or team[poke]["EVs"].get('SpD',0)>100:
+                team[poke]["Roles"].append("Recovery")
+        
+        #stat based shi
+        if team[poke]["EVs"].get("Atk", 0) >= 200:
+            team[poke]["Roles"].append("Physical Sweeper")
+        if team[poke]["EVs"].get("SpA", 0) >= 200:
+            team[poke]["Roles"].append("Special Sweeper")
+
+        #item based shi
+        item = team[poke]["Item"].lower()
+        if item in choice_items:
+            team[poke]["Roles"].append(choice_items[item])
+        if item=='Life Orb':
+            team[poke]["Roles"].append("Wallbreaker")
+        
+        
     return team
 
 
@@ -149,3 +148,6 @@ Timid Nature
 """ 
 team = readTeam(teamRaw)
 detectRole(team)
+
+for i in team:
+    print(team[i])
